d1864087c6071885f594941d5873b23bf6072d1d
DavideDaCol <118683352+DavideDaCol@users.noreply.github.com>
2024-03-04 11:32:56 +0100

    start implementing file handling

--- src/editor.cpp
+++ src/editor.cpp
@@ -1,4 +1,5 @@
 #include <ncurses.h>
+#include <fstream>
 #include <cstring>
 using namespace std;
 
@@ -11,16 +12,25 @@ int main(int argc, char** argv){
   getmaxyx(stdscr, y, x); // term size
   refresh();
 
-  const char * title = "PolyEdit: PolyML Editor";
-  const char * subtitle = "press ctrl+c to exit";
+  const char title[24] = "PolyEdit: PolyML Editor";
+  const char subtitle[33] = "press i to start, ctrl+c to exit";
 
   mvprintw(y/2,x/2-strlen(title)/2,title); //printw needs a const string
   mvprintw(y/2+1,x/2-strlen(subtitle)/2,subtitle);
   refresh();
 
-  char buff = getch(); 
+  char buff; 
+  do{
+    buff=getch();
+  } while(buff!='i');
+
+  fstream in,out;
+
+  if(argc == 2 ){ // gets file path
+    in.open(argv[1],ios::in);
+    out.open(argv[1],ios::out);
+  } else out.open("noname.txt",ios::out);
   clear();
-  printw("%c", buff); //TODO: learn this syntax
   getch(); // waits for a keypress to exit
   endwin(); //deallocates memory!
   return 0;
